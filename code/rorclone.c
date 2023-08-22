3180438d37e805b0b0d52b0f9d16c0052d5dfb58
Arseniy Khvorov <khvorov45@gmail.com>
2023-08-22 20:06:27 +1000

    winmain code cleanup

--- code/rorclone.c
+++ code/rorclone.c
@@ -62,34 +62,54 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
     unused(cmdline);
     unused(cmdshow);
 
-    WNDCLASSEXW windowClass = {
-        .cbSize = sizeof(windowClass),
-        .lpfnWndProc = windowProc,
-        .hInstance = instance,
-        .hIcon = LoadIcon(NULL, IDI_APPLICATION),
-        .hCursor = LoadCursor(NULL, IDC_ARROW),
-        .lpszClassName = L"rorclone_window_class",
-    };
+    struct {
+        HWND hwnd;
+        DWORD w, h;
+    } window = {};
 
     {
-        ATOM atom = RegisterClassExW(&windowClass);
-        assert(atom && "Failed to register window class");
+        WNDCLASSEXW windowClass = {
+            .cbSize = sizeof(windowClass),
+            .lpfnWndProc = windowProc,
+            .hInstance = instance,
+            .hIcon = LoadIcon(NULL, IDI_APPLICATION),
+            .hCursor = LoadCursor(NULL, IDC_ARROW),
+            .lpszClassName = L"rorclone_window_class",
+        };
+
+        {
+            ATOM atom = RegisterClassExW(&windowClass);
+            assert(atom && "Failed to register window class");
+        }
+
+        // NOTE(khvorov) According to Martins:
+        // WS_EX_NOREDIRECTIONBITMAP flag here is needed to fix ugly bug with Windows 10
+        // when window is resized and DXGI swap chain uses FLIP presentation model
+        window.hwnd = CreateWindowExW(
+            WS_EX_APPWINDOW | WS_EX_NOREDIRECTIONBITMAP,
+            windowClass.lpszClassName, L"rorclone",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            NULL, NULL, windowClass.hInstance, NULL
+        );
+        assert(window.hwnd && "Failed to create window");
     }
 
-    // NOTE(khvorov) According to Martins:
-    // WS_EX_NOREDIRECTIONBITMAP flag here is needed to fix ugly bug with Windows 10
-    // when window is resized and DXGI swap chain uses FLIP presentation model
-    HWND hwnd = CreateWindowExW(
-        WS_EX_APPWINDOW | WS_EX_NOREDIRECTIONBITMAP,
-        windowClass.lpszClassName, L"rorclone",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, NULL, windowClass.hInstance, NULL
-    );
-    assert(hwnd && "Failed to create window");
+    struct {
+        ID3D11Device* device;
+        ID3D11DeviceContext* context;
+        IDXGISwapChain1* swapChain;
+        ID3D11Buffer* vbuffer;
+        ID3D11InputLayout* layout;
+        ID3D11VertexShader* vshader;
+        ID3D11PixelShader* pshader;
+        ID3D11ShaderResourceView* textureView;
+        ID3D11SamplerState* sampler;
+        ID3D11BlendState* blendState;
+        ID3D11RasterizerState* rasterizerState;
+        ID3D11RenderTargetView* rtView;
+    } d3d11 = {};
 
-    ID3D11Device* device = 0;
-    ID3D11DeviceContext* context = 0;
     {
         UINT flags = 0;
         #ifndef NDEBUG
@@ -98,7 +118,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
         D3D_FEATURE_LEVEL levels[] = { D3D_FEATURE_LEVEL_11_0 };
         HRESULT D3D11CreateDeviceResult = D3D11CreateDevice(
             NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, flags, levels, arrayCount(levels),
-            D3D11_SDK_VERSION, &device, NULL, &context
+            D3D11_SDK_VERSION, &d3d11.device, NULL, &d3d11.context
         );
         assertHR(D3D11CreateDeviceResult);
     }
@@ -106,7 +126,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
     #ifndef NDEBUG
     {
         ID3D11InfoQueue* info;
-        ID3D11Device_QueryInterface(device, &IID_ID3D11InfoQueue, (void**)&info);
+        ID3D11Device_QueryInterface(d3d11.device, &IID_ID3D11InfoQueue, (void**)&info);
         ID3D11InfoQueue_SetBreakOnSeverity(info, D3D11_MESSAGE_SEVERITY_CORRUPTION, TRUE);
         ID3D11InfoQueue_SetBreakOnSeverity(info, D3D11_MESSAGE_SEVERITY_ERROR, TRUE);
         ID3D11InfoQueue_SetBreakOnSeverity(info, D3D11_MESSAGE_SEVERITY_WARNING, TRUE);
@@ -130,10 +150,9 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
     }
     #endif
 
-    IDXGISwapChain1* swapChain = 0;
     {
         IDXGIDevice* dxgiDevice = 0;
-        HRESULT ID3D11Device_QueryInterfaceResult = ID3D11Device_QueryInterface(device, &IID_IDXGIDevice, (void**)&dxgiDevice);
+        HRESULT ID3D11Device_QueryInterfaceResult = ID3D11Device_QueryInterface(d3d11.device, &IID_IDXGIDevice, (void**)&dxgiDevice);
         assertHR(ID3D11Device_QueryInterfaceResult);
 
         IDXGIAdapter* dxgiAdapter = 0;
@@ -153,10 +172,10 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
             .SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
         };
 
-        HRESULT IDXGIFactory2_CreateSwapChainForHwndResult = IDXGIFactory2_CreateSwapChainForHwnd(factory, (IUnknown*)device, hwnd, &desc, NULL, NULL, &swapChain);
+        HRESULT IDXGIFactory2_CreateSwapChainForHwndResult = IDXGIFactory2_CreateSwapChainForHwnd(factory, (IUnknown*)d3d11.device, window.hwnd, &desc, NULL, NULL, &d3d11.swapChain);
         assertHR(IDXGIFactory2_CreateSwapChainForHwndResult);
 
-        IDXGIFactory_MakeWindowAssociation(factory, hwnd, DXGI_MWA_NO_ALT_ENTER);
+        IDXGIFactory_MakeWindowAssociation(factory, window.hwnd, DXGI_MWA_NO_ALT_ENTER);
 
         IDXGIFactory2_Release(factory);
         IDXGIAdapter_Release(dxgiAdapter);
@@ -168,7 +187,6 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
         V2 scale;
     };
 
-    ID3D11Buffer* vbuffer = 0;
     {
         struct Vertex data[] = {
             {{0.0f, 0.0f}, .scale = {0.1, 0.1}},
@@ -183,28 +201,24 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
         };
 
         D3D11_SUBRESOURCE_DATA initial = { .pSysMem = data };
-        HRESULT ID3D11Device_CreateBufferResult = ID3D11Device_CreateBuffer(device, &desc, &initial, &vbuffer);
+        HRESULT ID3D11Device_CreateBufferResult = ID3D11Device_CreateBuffer(d3d11.device, &desc, &initial, &d3d11.vbuffer);
         assertHR(ID3D11Device_CreateBufferResult);
     }
 
-    ID3D11InputLayout* layout = 0;
-    ID3D11VertexShader* vshader = 0;
-    ID3D11PixelShader* pshader = 0;
     {
         D3D11_INPUT_ELEMENT_DESC desc[] = {
             {"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, offsetof(struct Vertex, pos),   D3D11_INPUT_PER_INSTANCE_DATA, 1 },
             {"SCALE",    0, DXGI_FORMAT_R32G32_FLOAT, 0, offsetof(struct Vertex, scale), D3D11_INPUT_PER_INSTANCE_DATA, 1 },
         };
 
-        HRESULT ID3D11Device_CreateVertexShaderResult = ID3D11Device_CreateVertexShader(device, globalShader_vs, sizeof(globalShader_vs), NULL, &vshader);
+        HRESULT ID3D11Device_CreateVertexShaderResult = ID3D11Device_CreateVertexShader(d3d11.device, globalShader_vs, sizeof(globalShader_vs), NULL, &d3d11.vshader);
         assertHR(ID3D11Device_CreateVertexShaderResult);
-        HRESULT ID3D11Device_CreatePixelShaderResult = ID3D11Device_CreatePixelShader(device, globalShader_ps, sizeof(globalShader_ps), NULL, &pshader);
+        HRESULT ID3D11Device_CreatePixelShaderResult = ID3D11Device_CreatePixelShader(d3d11.device, globalShader_ps, sizeof(globalShader_ps), NULL, &d3d11.pshader);
         assertHR(ID3D11Device_CreatePixelShaderResult);
-        HRESULT ID3D11Device_CreateInputLayoutResult = ID3D11Device_CreateInputLayout(device, desc, arrayCount(desc), globalShader_vs, sizeof(globalShader_vs), &layout);
+        HRESULT ID3D11Device_CreateInputLayoutResult = ID3D11Device_CreateInputLayout(d3d11.device, desc, arrayCount(desc), globalShader_vs, sizeof(globalShader_vs), &d3d11.layout);
         assertHR(ID3D11Device_CreateInputLayoutResult);
     }
 
-    ID3D11ShaderResourceView* textureView = 0;
     {
         u32 pixels[] = {
             0xff0000ff, 0xff00ff00,
@@ -230,12 +244,11 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
         };
 
         ID3D11Texture2D* texture;
-        ID3D11Device_CreateTexture2D(device, &desc, &data, &texture);
-        ID3D11Device_CreateShaderResourceView(device, (ID3D11Resource*)texture, NULL, &textureView);
+        ID3D11Device_CreateTexture2D(d3d11.device, &desc, &data, &texture);
+        ID3D11Device_CreateShaderResourceView(d3d11.device, (ID3D11Resource*)texture, NULL, &d3d11.textureView);
         ID3D11Texture2D_Release(texture);
     }
 
-    ID3D11SamplerState* sampler = 0;
     {
         D3D11_SAMPLER_DESC desc = {
             .Filter = D3D11_FILTER_MIN_MAG_MIP_POINT,
@@ -243,10 +256,9 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
             .AddressV = D3D11_TEXTURE_ADDRESS_WRAP,
             .AddressW = D3D11_TEXTURE_ADDRESS_WRAP,
         };
-        ID3D11Device_CreateSamplerState(device, &desc, &sampler);
+        ID3D11Device_CreateSamplerState(d3d11.device, &desc, &d3d11.sampler);
     }
 
-    ID3D11BlendState* blendState = 0;
     {
         D3D11_BLEND_DESC desc = {
             .RenderTarget[0] = {
@@ -260,126 +272,139 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previnstance, LPSTR cmdline, in
                 .RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL,
             },
         };
-        ID3D11Device_CreateBlendState(device, &desc, &blendState);
+        ID3D11Device_CreateBlendState(d3d11.device, &desc, &d3d11.blendState);
     }
 
-    ID3D11RasterizerState* rasterizerState = 0;
     {
         D3D11_RASTERIZER_DESC desc = {
             .FillMode = D3D11_FILL_SOLID,
             .CullMode = D3D11_CULL_NONE,
         };
-        ID3D11Device_CreateRasterizerState(device, &desc, &rasterizerState);
+        ID3D11Device_CreateRasterizerState(d3d11.device, &desc, &d3d11.rasterizerState);
     }
 
-    ID3D11RenderTargetView* rtView = NULL;
-
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
-
-    DWORD currentWidth = 0;
-    DWORD currentHeight = 0;
+    ShowWindow(window.hwnd, SW_SHOWDEFAULT);
 
     for (;;) {
-        MSG msg = {};
-        if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+
         {
-            if (msg.message == WM_QUIT)
-            {
-                break;
+            MSG msg = {};
+            if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
+                if (msg.message == WM_QUIT) {
+                    break;
+                }
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+                continue;
             }
-            TranslateMessage(&msg);
-            DispatchMessageW(&msg);
-            continue;
         }
 
-        // get current size for window client area
-        RECT rect = {};
-        GetClientRect(hwnd, &rect);
-        DWORD windowWidth = rect.right - rect.left;
-        DWORD windowHeight = rect.bottom - rect.top;
+        {
+            RECT rect = {};
+            GetClientRect(window.hwnd, &rect);
+            DWORD windowWidth = rect.right - rect.left;
+            DWORD windowHeight = rect.bottom - rect.top;
 
-        if (rtView == NULL || windowWidth != currentWidth || windowHeight != currentHeight) {
-            if (rtView) {
-                ID3D11DeviceContext_ClearState(context);
-                ID3D11RenderTargetView_Release(rtView);
-                rtView = NULL;
-            }
-
-            if (windowWidth != 0 && windowHeight != 0) {
-                HRESULT IDXGISwapChain1_ResizeBuffersResult = IDXGISwapChain1_ResizeBuffers(swapChain, 0, windowWidth, windowHeight, DXGI_FORMAT_UNKNOWN, 0);
-                if (FAILED(IDXGISwapChain1_ResizeBuffersResult)) {
-                    fatalError("Failed to resize swap chain!");
+            if (d3d11.rtView == NULL || windowWidth != window.w || windowHeight != window.h) {
+                if (d3d11.rtView) {
+                    ID3D11DeviceContext_ClearState(d3d11.context);
+                    ID3D11RenderTargetView_Release(d3d11.rtView);
+                    d3d11.rtView = NULL;
                 }
 
-                ID3D11Texture2D* backbuffer = 0;
-                IDXGISwapChain1_GetBuffer(swapChain, 0, &IID_ID3D11Texture2D, (void**)&backbuffer);
-                ID3D11Device_CreateRenderTargetView(device, (ID3D11Resource*)backbuffer, NULL, &rtView);
-                ID3D11Texture2D_Release(backbuffer);
+                if (windowWidth != 0 && windowHeight != 0) {
+                    HRESULT IDXGISwapChain1_ResizeBuffersResult = IDXGISwapChain1_ResizeBuffers(d3d11.swapChain, 0, windowWidth, windowHeight, DXGI_FORMAT_UNKNOWN, 0);
+                    if (FAILED(IDXGISwapChain1_ResizeBuffersResult)) {
+                        fatalError("Failed to resize swap chain!");
+                    }
 
-                D3D11_TEXTURE2D_DESC depthDesc = {
-                    .Width = windowWidth,
-                    .Height = windowHeight,
-                    .MipLevels = 1,
-                    .ArraySize = 1,
-                    .Format = DXGI_FORMAT_D32_FLOAT, // or use DXGI_FORMAT_D32_FLOAT_S8X24_UINT if you need stencil
-                    .SampleDesc = { 1, 0 },
-                    .Usage = D3D11_USAGE_DEFAULT,
-                    .BindFlags = D3D11_BIND_DEPTH_STENCIL,
-                };
+                    ID3D11Texture2D* backbuffer = 0;
+                    IDXGISwapChain1_GetBuffer(d3d11.swapChain, 0, &IID_ID3D11Texture2D, (void**)&backbuffer);
+                    ID3D11Device_CreateRenderTargetView(d3d11.device, (ID3D11Resource*)backbuffer, NULL, &d3d11.rtView);
+                    ID3D11Texture2D_Release(backbuffer);
 
-                ID3D11Texture2D* depth;
-                ID3D11Device_CreateTexture2D(device, &depthDesc, NULL, &depth);
-                ID3D11Texture2D_Release(depth);
+                    D3D11_TEXTURE2D_DESC depthDesc = {
+                        .Width = windowWidth,
+                        .Height = windowHeight,
+                        .MipLevels = 1,
+                        .ArraySize = 1,
+                        .Format = DXGI_FORMAT_D32_FLOAT, // or use DXGI_FORMAT_D32_FLOAT_S8X24_UINT if you need stencil
+                        .SampleDesc = { 1, 0 },
+                        .Usage = D3D11_USAGE_DEFAULT,
+                        .BindFlags = D3D11_BIND_DEPTH_STENCIL,
+                    };
+
+                    ID3D11Texture2D* depth;
+                    ID3D11Device_CreateTexture2D(d3d11.device, &depthDesc, NULL, &depth);
+                    ID3D11Texture2D_Release(depth);
+                }
+
+                window.w = windowWidth;
+                window.h = windowHeight;
             }
-
-            currentWidth = windowWidth;
-            currentHeight = windowHeight;
         }
 
-        if (rtView) {
+        if (d3d11.rtView) {
             D3D11_VIEWPORT viewport = {
                 .TopLeftX = 0,
                 .TopLeftY = 0,
-                .Width = (FLOAT)windowWidth,
-                .Height = (FLOAT)windowHeight,
+                .Width = (FLOAT)window.w,
+                .Height = (FLOAT)window.h,
                 .MinDepth = 0,
                 .MaxDepth = 1,
             };
 
             FLOAT color[] = {0, 0, 0, 0};
-            ID3D11DeviceContext_ClearRenderTargetView(context, rtView, color);
+            ID3D11DeviceContext_ClearRenderTargetView(d3d11.context, d3d11.rtView, color);
 
-            ID3D11DeviceContext_IASetInputLayout(context, layout);
-            ID3D11DeviceContext_IASetPrimitiveTopology(context, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+            ID3D11DeviceContext_IASetInputLayout(d3d11.context, d3d11.layout);
+            ID3D11DeviceContext_IASetPrimitiveTopology(d3d11.context, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
             {
                 UINT stride = sizeof(struct Vertex);
                 UINT offset = 0;
-                ID3D11DeviceContext_IASetVertexBuffers(context, 0, 1, &vbuffer, &stride, &offset);
+                ID3D11DeviceContext_IASetVertexBuffers(d3d11.context, 0, 1, &d3d11.vbuffer, &stride, &offset);
             }
 
-            ID3D11DeviceContext_VSSetShader(context, vshader, NULL, 0);
+            ID3D11DeviceContext_VSSetShader(d3d11.context, d3d11.vshader, NULL, 0);
 
-            ID3D11DeviceContext_RSSetViewports(context, 1, &viewport);
-            ID3D11DeviceContext_RSSetState(context, rasterizerState);
+            ID3D11DeviceContext_RSSetViewports(d3d11.context, 1, &viewport);
+            ID3D11DeviceContext_RSSetState(d3d11.context, d3d11.rasterizerState);
 
-            ID3D11DeviceContext_PSSetSamplers(context, 0, 1, &sampler);
-            ID3D11DeviceContext_PSSetShaderResources(context, 0, 1, &textureView);
-            ID3D11DeviceContext_PSSetShader(context, pshader, NULL, 0);
+            ID3D11DeviceContext_PSSetSamplers(d3d11.context, 0, 1, &d3d11.sampler);
+            ID3D11DeviceContext_PSSetShaderResources(d3d11.context, 0, 1, &d3d11.textureView);
+            ID3D11DeviceContext_PSSetShader(d3d11.context, d3d11.pshader, NULL, 0);
 
-            ID3D11DeviceContext_OMSetBlendState(context, blendState, NULL, ~0U);
-            ID3D11DeviceContext_OMSetRenderTargets(context, 1, &rtView, 0);
+            ID3D11DeviceContext_OMSetBlendState(d3d11.context, d3d11.blendState, NULL, ~0U);
+            ID3D11DeviceContext_OMSetRenderTargets(d3d11.context, 1, &d3d11.rtView, 0);
 
-            ID3D11DeviceContext_DrawInstanced(context, 4, 2, 0, 0);
+            ID3D11DeviceContext_DrawInstanced(d3d11.context, 4, 2, 0, 0);
         }
 
-        BOOL vsync = TRUE;
-        HRESULT IDXGISwapChain1_PresentResult = IDXGISwapChain1_Present(swapChain, vsync ? 1 : 0, 0);
-        if (IDXGISwapChain1_PresentResult == DXGI_STATUS_OCCLUDED) {
-            if (vsync) {
-                Sleep(10);
+        {
+            BOOL vsync = TRUE;
+            HRESULT IDXGISwapChain1_PresentResult = IDXGISwapChain1_Present(d3d11.swapChain, vsync ? 1 : 0, 0);
+            if (IDXGISwapChain1_PresentResult == DXGI_STATUS_OCCLUDED) {
+                if (vsync) {
+                    Sleep(10);
+                }
+            } else if (FAILED(IDXGISwapChain1_PresentResult)) {
+                fatalError("Failed to present swap chain! Device lost?");
             }
-        } else if (FAILED(IDXGISwapChain1_PresentResult)) {
-            fatalError("Failed to present swap chain! Device lost?");
         }
     }
+
+    d3d11.device->lpVtbl->Release(d3d11.device);
+    d3d11.context->lpVtbl->Release(d3d11.context);
+    d3d11.swapChain->lpVtbl->Release(d3d11.swapChain);
+    d3d11.vbuffer->lpVtbl->Release(d3d11.vbuffer);
+    d3d11.layout->lpVtbl->Release(d3d11.layout);
+    d3d11.vshader->lpVtbl->Release(d3d11.vshader);
+    d3d11.pshader->lpVtbl->Release(d3d11.pshader);
+    d3d11.textureView->lpVtbl->Release(d3d11.textureView);
+    d3d11.sampler->lpVtbl->Release(d3d11.sampler);
+    d3d11.blendState->lpVtbl->Release(d3d11.blendState);
+    d3d11.rasterizerState->lpVtbl->Release(d3d11.rasterizerState);
+    d3d11.rtView->lpVtbl->Release(d3d11.rtView);
+
+    return 0;
 }
