0537133693c47b7c22b1ddbf47e6db72b0ef010a
Arseniy Khvorov <khvorov45@gmail.com>
2023-09-11 14:47:44 +1000

    pull out common code

--- code/build.c
+++ code/build.c
@@ -1,127 +1,10 @@
-#pragma clang diagnostic ignored "-Wunused-function"
-#pragma clang diagnostic ignored "-Wgnu-variable-sized-type-not-at-end"
-
-#include <stdint.h>
-#include <stddef.h>
-
-#define STB_SPRINTF_STATIC
-#define STB_SPRINTF_IMPLEMENTATION
-#include "stb_sprintf.h"
-
 #define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include <windows.h>
+#define assert(cond) do { if (cond) {} else {char msg[] = __FILE__ ":" STRINGIFY(__LINE__) ":1: error: assertion failure\n"; WriteFile((HANDLE)STD_OUTPUT_HANDLE, msg, sizeof(msg) - 1, 0, 0); __debugbreak();} } while (0)
+#include "common.c"
 
-#define Byte 1
-#define Kilobyte 1024 * Byte
-#define Megabyte 1024 * Kilobyte
-#define Gigabyte 1024 * Megabyte
+#include <stdlib.h>
 
-#define STRINGIFY_(x) #x
-#define STRINGIFY(x) STRINGIFY_(x)
-#define assert(cond) do { if (cond) {} else {writeToStdout(STR(__FILE__ ":" STRINGIFY(__LINE__) ":1: error: assertion failure\n")); __debugbreak();} } while (0)
-#define STR(x) ((Str){x, sizeof(x) - 1})
-#define LIT(x) (int)x.len, x.ptr
-#define unused(x) ((x) = (x))
-#define arrayCount(x) (sizeof(x) / sizeof((x)[0]))
-#define arrpush(arr, val) assert((arr).len < (arr).cap); (arr).ptr[(arr).len++] = (val)
-#define arenaAllocArray(arena, type, count) ((type*)arenaAlloc((arena), sizeof(type) * (count)))
-#define tempMemoryBlock(arena_) for (TempMemory _temp_ = beginTempMemory(arena_); _temp_.arena; endTempMemory(&_temp_))
-
-typedef int8_t   i8;
-typedef uint8_t  u8;
-typedef int16_t  i16;
-typedef uint16_t u16;
-typedef int32_t  i32;
-typedef uint32_t u32;
-typedef int64_t  i64;
-typedef uint64_t u64;
-typedef float    f32;
-typedef double   f64;
-
-typedef struct {u8* ptr; i64 len;} u8arr;
-
-typedef struct Str {
-    char* ptr;
-    i64 len;
-} Str;
-
-static void writeToStdout(Str str) {
-    WriteFile((HANDLE)STD_OUTPUT_HANDLE, str.ptr, str.len, 0, 0);
-}
-
-typedef struct Arena {
-    void* base;
-    i64 size;
-    i64 used;
-    i64 tempCount;
-} Arena;
-static i64 arenaFreesize(Arena* arena) { return arena->size - arena->used;}
-static void* arenaFreeptr(Arena* arena) { return arena->base + arena->used;}
-
-static void* arenaAlloc(Arena* arena, i64 size) {
-    assert(arenaFreesize(arena) >= size);
-    void* result = arenaFreeptr(arena);
-    arena->used += size;
-    return result;
-}
-
-typedef struct TempMemory {
-    i64 usedBefore;
-    i64 tempBefore;
-    Arena* arena;
-} TempMemory;
-
-static TempMemory beginTempMemory(Arena* arena) {
-    TempMemory result = {.usedBefore = arena->used, .tempBefore = arena->tempCount, .arena = arena};
-    arena->tempCount += 1;
-    return result;
-}
-
-static void endTempMemory(TempMemory* temp) {
-    assert(temp->usedBefore <= temp->arena->used);
-    assert(temp->tempBefore == temp->arena->tempCount - 1);
-    temp->arena->used = temp->usedBefore;
-    temp->arena->tempCount -= 1;
-    *temp = (TempMemory) {};
-}
-
-static bool memeq(void* ptr1, void* ptr2, i64 len) {
-    int memcmpResult = memcmp(ptr1, ptr2, len);
-    bool result = memcmpResult == 0;
-    return result;
-}
-
-STBSP__ATTRIBUTE_FORMAT(2,3)
-static Str strfmt(Arena* arena, char* fmt, ...) {
-    char* out = arenaFreeptr(arena);
-
-    va_list va;
-    va_start(va, fmt);
-    int printResult = stbsp_vsnprintf(out, arenaFreesize(arena), fmt, va);
-    va_end(va);
-
-    arena->used += printResult + 1;
-    Str result = {out, printResult};
-    return result;
-}
-
-static bool streq(Str str1, Str str2) {
-    bool result = false;
-    if (str1.len == str2.len) {
-        result = memeq(str1.ptr, str2.ptr, str1.len);
-    }
-    return result;
-}
-
-static bool strstarts(Str str, Str start) {
-    bool result = false;
-    if (str.len >= start.len) {
-        result = memeq(str.ptr, start.ptr, start.len);
-    }
-    return result;
-}
-
-static Str strslice(Str str, i64 start, i64 end) {return (Str) {str.ptr + start, end - start};}
 static char capitalize(char ch) {return ch - ('a' - 'A');}
 
 typedef struct StrBuilder {
@@ -131,7 +14,7 @@ typedef struct StrBuilder {
     Str currentName;
 } StrBuilder;
 
-STBSP__ATTRIBUTE_FORMAT(2,3)
+__attribute__((format(printf,2,3)))
 static void builderfmt(StrBuilder* builder, char* fmt, ...) {
     char* out = builder->ptr + builder->len;
     i64 size = builder->cap - builder->len;
@@ -170,39 +53,6 @@ static void builderTableEnd(StrBuilder* builder) {
     builderfmt(builder, "};\n\n");
 }
 
-static u8arr readEntireFile(Arena* arena, Str path) {
-    HANDLE hfile = 0;
-    tempMemoryBlock(arena) {
-        Str path0 = strfmt(arena, "%.*s", LIT(path));
-
-        hfile = CreateFileA(
-            path0.ptr,
-            GENERIC_READ,
-            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-            0,
-            OPEN_EXISTING,
-            FILE_ATTRIBUTE_NORMAL,
-            0
-        );
-        assert(hfile != INVALID_HANDLE_VALUE);
-    }
-
-    LARGE_INTEGER fileSize = {};
-    BOOL GetFileSizeExResult = GetFileSizeEx(hfile, &fileSize);
-    assert(GetFileSizeExResult);
-
-    void* fileContent = arenaAllocArray(arena, u8, fileSize.QuadPart);
-    DWORD bytesRead = 0;
-    BOOL ReadFileResult = ReadFile(hfile, fileContent, fileSize.QuadPart, &bytesRead, 0);
-    assert(ReadFileResult);
-    assert(bytesRead == fileSize.QuadPart);
-
-    CloseHandle(hfile);
-
-    u8arr result = {fileContent, fileSize.QuadPart};
-    return result;
-}
-
 static void writeEntireFile(Arena* arena, Str path, void* ptr, i64 len) {
     HANDLE hfile = 0;
     tempMemoryBlock(arena) {
@@ -227,148 +77,6 @@ static void writeEntireFile(Arena* arena, Str path, void* ptr, i64 len) {
 
     CloseHandle(hfile);
 }
-
-
-//
-// SECTION Aseprite
-//
-
-#pragma pack(push)
-#pragma pack(1)
-
-typedef enum AseChunkType: u16 {
-    AseChunkType_OldPalette = 0x0004,
-    AseChunkType_Layer = 0x2004,
-    AseChunkType_Cel = 0x2005,
-    AseChunkType_ColorProfile = 0x2007,
-    AseChunkType_Palette = 0x2019,
-} AseChunkType;
-
-typedef struct Fixed16x16 {
-    u16 p1;
-    u16 p2;
-} Fixed16x16;
-
-typedef enum AseColorProfileType: u16 {
-    AseColorProfileType_None = 0,
-    AseColorProfileType_sRGB = 1,
-    AseColorProfileType_ICC = 2,
-} AseColorProfileType;
-
-typedef struct AseChunkColorProfile {
-    AseColorProfileType type;
-    u16 flags;
-    Fixed16x16 gamma;
-    u8 reserved[8];
-    struct {u32 len; void* ptr;} icc;
-} AseChunkColorProfile;
-
-typedef struct AseString {
-    u16 len;
-    char str[];
-} AseString;
-
-typedef enum AseLayerFlag {
-    AseLayerFlag_Visible = 1,
-    AseLayerFlag_Editable = 2,
-    AseLayerFlag_LockMovement = 4,
-    AseLayerFlag_Background = 8,
-    AseLayerFlag_PreferLinkedCels = 16,
-    AseLayerFlag_LayerGroupShouldBeDisplayedCollapsed = 32,
-    AseLayerFlag_LayerIsAReferenceLayer = 64,
-} AseLayerFlag;
-typedef u16 AseLayerFlags;
-
-typedef enum AseLayerType: u16 {
-    AseLayerType_Normal = 0,
-    AseLayerType_Group = 1,
-    AseLayerType_Tilemap = 2,
-} AseLayerType;
-
-typedef struct AseChunkLayer {
-    AseLayerFlags flags;
-    AseLayerType type;
-    u16 childLevel;
-    u16 defaultWidthIgnored;
-    u16 defaultHeightIgnored;
-    u16 blendMode;
-    u8 opacity;
-    u8 future[3];
-    AseString name;
-    u16 tilesetIndex;
-} AseChunkLayer;
-
-typedef enum AseCelType: u16 {
-    AseCelType_Raw = 0,
-    AseCelType_Linked = 1,
-    AseCelType_CompressedImage = 2,
-    AseCelType_CompressedTilemap = 3,
-} AseCelType;
-
-typedef struct AseChunkCel {
-    u16 index;
-    i16 posX;
-    i16 posY;
-    u8 opacity;
-    AseCelType type;
-    i16 zIndexOffset;
-    u8 future[5];
-    u16 width;
-    u16 height;
-    u8 compressed[];
-} AseChunkCel;
-
-typedef struct AseChunk {
-    u32 size;
-    AseChunkType type;
-    union {
-        AseChunkColorProfile colorProfile;
-        AseChunkLayer layer;
-        AseChunkCel cel;
-    };
-} AseChunk;
-
-typedef struct AseFrame {
-    u32 bytes;
-    u16 magic; // 0xF1FA
-    u16 chunkCountOld;
-    u16 frameDurationMS;
-    u8 forFuture[2];
-    u32 chunksCountNew;
-    AseChunk chunks[];
-} AseFrame;
-
-typedef struct AseFile {
-    u32 fileSize;
-    u16 magic; // 0xA5E0
-    u16 frameCount;
-    u16 width;
-    u16 height;
-    u16 bitsPerPixel;
-    u32 flags;
-    u16 msBetweenFramesDepricated;
-    u32 zero1;
-    u32 zero2;
-    u8 palleteEntryIndexForTransparentColor;
-    u8 ignore[3];
-    u16 colorCount; // 0 means 256 for old sprites
-    u8 pixelWidth;
-    u8 pixelHeight;
-    i16 gridXPos;
-    i16 gridYPos;
-    u16 gridWidth;
-    u16 gridHeight;
-    u8 forFuture[84];
-    AseFrame frames[];
-} AseFile;
-
-#pragma pack(pop)
-
-//
-// SECTION Main
-//
-
-#include <stdlib.h>
 
 typedef struct FileInfo {
     struct {Str entity, animation, file;} names;
@@ -479,14 +187,11 @@ int main() {
             info->content = ase;
 
             #if 0
-            animation->frameDurationInMS = arenaAllocArray(memory.perm, f32, ase->frameCount);
-
             AseFrame* frame = ase->frames;
             V2 firstFrameArtOffset = {};
 
             for (i32 frameIndex = 0; frameIndex < ase->frameCount; frameIndex++) {
                 assert(frame->magic == 0xF1FA);
-                animation->frameDurationInMS[frameIndex] = frame->frameDurationMS;
 
                 AseChunk* chunk = frame->chunks;
                 AtlasID thisID = getAtlasID(thisEntityID, thisAnimationID, frameIndex);
